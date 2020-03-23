0e9f8df81c385eb1f4699fb6632d54a6a484c9f7
West Kraemer <kraemerwest@gmail.com>
2020-03-23 17:18:13 +0100

    control flow review in python3

---
+++ flow.py
@@ -0,0 +1,22 @@
+""" spam = 0
+while spam < 5:
+    print("spam")
+    spam = spam + 1
+ """
+
+""" Shift + Option + A  """
+
+
+""" name = ''
+while not name:
+    print("Enter your name")
+    name = input()
+print("how many guests?")
+numGuests = int(input())
+if numGuests:
+    print(f"you have {numGuests} comming")
+print("done") """
+
+print('My name is')
+for i in range(5):
+    print('Jimmy Five Times (' + str(i+1) + ')')
